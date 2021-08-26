4681536606c27489302adb69dc8cbd1b732acb48
André Farias <andre.farias@fitbank.com.br>
2021-08-26 10:52:40 -0300

    Projeto de Calculadora Simples em c#

---
+++ TesteGit/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TesteGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro numero: ");
+            double n1= double.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo numero: ");
+            double n2= double.Parse(Console.ReadLine());
+            Console.Write("Digite o simbolo da operação:");
+            string op = Console.ReadLine();
+
+            double r;
+            switch (op)
+            {
+                
+                case "+":
+                    r = n1 + n2;
+                       Console.WriteLine(r);
+                    break;
+                case "/":
+                    r = n1 / n2;
+                    Console.WriteLine(r);
+                    break;
+                case "-":
+                    r = n1 - n2;
+                    Console.WriteLine(r);
+                    break;
+                case "*":
+                    r = n1 * n2;
+                    Console.WriteLine(r);
+                    break;
+
+
+
+            }
+        }
+    }
+}
